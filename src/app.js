1c59c8f15e629a3362fb45bdce1cdb0f6cf8595b
Naren <naren@mac.home>
2025-10-02 15:27:44 -0400

    changes made to http methods

--- src/app.js
+++ src/app.js
@@ -1,10 +1,6 @@
 const express=require("express");
 const app=express();
-app.use("/test",function(req,res){
-res.send("This is a response from test page, conected wow, yahoo!!!");
-
-});
-
+/*
 app.use("/home",function(req,res){
 res.send("This is a response from home page, conected wow, yahoo!!!");
 
@@ -14,6 +10,26 @@ app.use("/",function(req,res){
 res.send("This is a response from default page, conected wow, yahoo!!!");
 
 });
+app.use("/test",function(req,res){
+res.send("This is a response from test page, conected wow, yahoo!!!");
+
+});
+*/
+app.get("/user",function(req,res){
+    res.send({firstName:"Naren", lastName:"Siriki"});
+
+});
+app.post("/user",function(req,res){
+    res.send("Data sent successfully");
+
+});
+app.delete("/user",function(req,res){
+    res.send("Data deleted successfully");
+
+});
+
+
+
 
 app.listen(7777, function(){
     console.log("Server is listening on port 7777 successfully");
