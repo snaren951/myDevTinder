540fef579ffd3cfbca9b8cc5c9bb90a673b55b00
Naren <naren@mac.home>
2025-10-03 14:39:27 -0400

    Route hanlders and next

--- src/app.js
+++ src/app.js
@@ -15,8 +15,14 @@ res.send("This is a response from test page, conected wow, yahoo!!!");
 
 });
 */
+app.get("/abcd",function(req,res){
+    res.send({firstName:"ABC", lastName:"DEF"});
+    console.log(req.query);
+
+});
+
 app.get("/user",function(req,res){
-    res.send({firstName:"Naren", lastName:"Siriki"});
+    res.send({firstName:"Naren", lastName:"S"});
 
 });
 app.post("/user",function(req,res){
@@ -27,7 +33,28 @@ app.delete("/user",function(req,res){
     res.send("Data deleted successfully");
 
 });
+app.get("/data",[function(req,res,next){
+    console.log("Route Handler 1");
+    //res.send("Route 1 response");
+    next();
 
+},
+function(req,res,next){
+console.log("Route Handler 2");
+    //res.send("Route 2 response");
+      next();
+},
+function(req,res,next){
+console.log("Route Handler 3");
+    //res.send("Route 3 response");
+      next();
+}],
+function(req,res,next){
+    console.log("Route Handler 4");
+    res.send("Route 4 response");
+    //next();
+
+});
 
 
 
